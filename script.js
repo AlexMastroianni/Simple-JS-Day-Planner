0c5f79e1e9cada2334d43a27a0585a0c5fd6d5de
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-08-29 12:24:59 +1000

    sudo code for js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var currentTime = document.querySelector("#currentDay");
-
-// starts and stops countdown
-function clockUpadte() {
-  timer = setInterval(function () {
-    currentTime.textContent = moment().format("MMMM Do YYYY, h:mm:ss a");
-  }, 1000);
-}
+var containertEl = $(".container");
+var userInput =
+  // starts and stops countdown
+  function clockUpadte() {
+    timer = setInterval(function () {
+      currentTime.textContent = moment().format("MMMM Do YYYY, h:mm:ss a");
+    }, 1000);
+  };
 clockUpadte();
+
+// code to save user input into local store off save button
+
+// code to check current time and add/remove style elements to user
