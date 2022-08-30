d0951984bd26ce8492a729f16b8a5eabe09a1b8a
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-08-31 08:49:27 +1000

    housekeeping, added comments, MVP ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var currentTime = document.querySelector("#currentDay");
 var currentHour = moment().hour();
 
-// starts and stops countdown
+// Updates seconds on timeclock
 function clockUpdate() {
   timer = setInterval(function () {
     currentTime.textContent = moment().format("MMMM Do YYYY, h:mm:ss a");
@@ -9,11 +9,14 @@ function clockUpdate() {
 }
 clockUpdate();
 
+// targets each timeblock class, var 'hour' targerts with ID being an hour by number, userInput grabs the text field info from local storage based on teh ID
 $(".time-block").each(function () {
   var hour = $(this).attr("id");
   var userInput = localStorage.getItem(hour);
   var description = $(this).find(".description");
+  // updates the text are with the data from local storage
   description.val(userInput);
+  // checks what the current time is and adds and removes classes
   if (hour < currentHour) {
     $(this).find(".description").addClass("past");
   } else if (hour == currentHour) {
@@ -23,8 +26,9 @@ $(".time-block").each(function () {
   }
 });
 
+// save button adds the text area data to local storage using the current hour ID as the keyname
 $(".saveBtn").on("click", function () {
   var localKey = $(this).parent().attr("id");
-  var value = $(this).parent().find(".description").val();
-  localStorage.setItem(localKey, value);
+  var description = $(this).parent().find(".description").val();
+  localStorage.setItem(localKey, description);
 });
